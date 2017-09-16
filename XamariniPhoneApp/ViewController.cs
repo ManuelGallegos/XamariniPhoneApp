44c5ab7dfdb9242b62e7d1037140ed9a5af3f3bc
ManuelGallegos <manuel_mty@yahoo.com>
2017-09-16 12:41:03 -0400

    Add project files.

---
+++ XamariniPhoneApp/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamariniPhoneApp
+{
+    public partial class ViewController : UIViewController
+    {
+
+
+        int iCounter = 0;
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void BtnHello_TouchUpInside(UIButton sender)
+        {
+            lblHello.Text = string.Format( "Number of clicks {0}", ++iCounter);
+        }
+    }
+}
---
+++ XamariniPhoneApp/ViewController.designer.cs
@@ -0,0 +1,41 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamariniPhoneApp
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnHello { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblHello { get; set; }
+
+        [Action ("BtnHello_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnHello_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnHello != null) {
+                btnHello.Dispose ();
+                btnHello = null;
+            }
+
+            if (lblHello != null) {
+                lblHello.Dispose ();
+                lblHello = null;
+            }
+        }
+    }
+}
